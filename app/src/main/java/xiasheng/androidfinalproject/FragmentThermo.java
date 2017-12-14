2eda03c6fece1348d1776af1166a3acb21a8e71b
share <shen0083@algonquinlive.com>
2017-12-14 15:42:33 -0500

    ProgressBar,AsyncTask,snackbar

--- app/src/main/java/xiasheng/androidfinalproject/FragmentThermo.java
+++ app/src/main/java/xiasheng/androidfinalproject/FragmentThermo.java
@@ -36,35 +36,35 @@ public class FragmentThermo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
        View view = inflater.inflate(R.layout.fragment_thermo_edit, container, false);
 
-        dbHelper = new House_DatabaseHelper(getActivity());
-        dbHelper.openDatabase();
-        Bundle bundle = this.getArguments();
-         id = bundle.getLong("id");
-         day = bundle.getString("day");
-         hour = bundle.getString("hour");
-        minute = bundle.getString("minute");
-        temp = bundle.getString("temperature");
-
-        textViewId=view.findViewById(R.id.fragmentId);
-        textViewId.setText("ID="+id);
-        textViewDay=view.findViewById(R.id.fragmentWeek);
-        textViewDay.setText("Day="+day);
-        textViewHour=view.findViewById(R.id.fragmentHour);
-        textViewHour.setText("Hour="+hour);
-        textViewMinute=view.findViewById(R.id.fragmentMinute);
-        textViewMinute.setText("Minute="+minute);
-        textViewTemp=view.findViewById(R.id.fragmentTemp);
-        textViewTemp.setText("Temperature="+temp);
-
-        del = view.findViewById(R.id.button_delete_fg_h);
-        del.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent();
-                intent.putExtra("id",id);
-                getActivity().setResult(Activity.RESULT_OK,intent);
-                getActivity().finish();
-            }
-        });
+//        dbHelper = new House_DatabaseHelper(getActivity());
+//        dbHelper.openDatabase();
+//        Bundle bundle = this.getArguments();
+//         id = bundle.getLong("id");
+//         day = bundle.getString("day");
+//         hour = bundle.getString("hour");
+//        minute = bundle.getString("minute");
+//        temp = bundle.getString("temperature");
+//
+//        textViewId=view.findViewById(R.id.fragmentId);
+//        textViewId.setText("ID="+id);
+//        textViewDay=view.findViewById(R.id.fragmentWeek);
+//        textViewDay.setText("Day="+day);
+//        textViewHour=view.findViewById(R.id.fragmentHour);
+//        textViewHour.setText("Hour="+hour);
+//        textViewMinute=view.findViewById(R.id.fragmentMinute);
+//        textViewMinute.setText("Minute="+minute);
+//        textViewTemp=view.findViewById(R.id.fragmentTemp);
+//        textViewTemp.setText("Temperature="+temp);
+//
+//        del = view.findViewById(R.id.button_delete_fg_h);
+//        del.setOnClickListener(new View.OnClickListener(){
+//            public void onClick(View v){
+//                Intent intent = new Intent();
+//                intent.putExtra("id",id);
+//                getActivity().setResult(Activity.RESULT_OK,intent);
+//                getActivity().finish();
+//            }
+//        });
         return view;
     }
 
